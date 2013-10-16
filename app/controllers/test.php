a5e3f4ed6de6f325724d9376809440d92f8ed6f9
Tuxity <kevin.darcel@gmail.com>
2013-10-16 20:20:36 +0200

    cleanup

--- app/controllers/test.php
+++ app/controllers/test.php
@@ -4,7 +4,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ParameterBag;
 
-$app->get("/api/", function (Request $request) use ($app) {
+$app->get("/", function (Request $request) use ($app) {
 
-    return "return bidule";
+    return "return ok";
 });
