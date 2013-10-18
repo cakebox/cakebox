dc6f55c80fee96807d4f6d85534a25bfb7845c69
Tuxity <kevin.darcel@gmail.com>
2013-10-18 19:56:04 +0200

    Add breadcrumbs

--- public/js/app.js
+++ public/js/app.js
@@ -4,12 +4,26 @@ app.controller('MainCtrl', function($scope, $http, $location) {
 
 	$scope.currentPath = "";
 	$scope.location = $location;
+    $scope.breadcrumbs = [];
 
 	$scope.$watch("location.path()", function(path) {
 
 		$scope.currentPath = path;
 		$http.get('/api/directories' + $scope.currentPath).success(function(data) {
-			scope.dirs = data;
+			$scope.dirs = data;
 		});
-	})
+
+        // Parse breadcrumbs
+        var pathElements = path.split('/'), result = [], i;
+        var breadcrumbPath = function (index) {
+            return '/' + (pathElements.slice(0, index + 1)).join('/');
+        };
+        result.push({name: ".", path: "."});
+        pathElements.shift();
+        for (i=0; i<pathElements.length; i++) {
+            result.push({name: pathElements[i], path: breadcrumbPath(i)});
+        }
+
+        $scope.breadcrumbs = result;
+	});
 });
