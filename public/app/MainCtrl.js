9f65e8d9b12e726e477dd08e58135dd8ab74cbc0
Tuxity <kevin.darcel@gmail.com>
2013-10-19 17:39:33 +0200

    Dont call api on /about /contact

--- public/app/MainCtrl.js
+++ public/app/MainCtrl.js
@@ -9,22 +9,27 @@ cakeboxCtrl.controller('MainCtrl', ['$scope', '$http', '$location',
 
         $scope.$watch("location.path()", function(path) {
 
-            $scope.currentPath = path;
-            $http.get('/api/directories' + $scope.currentPath).success(function(data) {
+            // ugly I think, maybe there is a better way
+            if (path != "/contact" && path !="/about")
+            {
+                $scope.currentPath = path;
+                $http.get('/api/directories' + $scope.currentPath).success(function(data) {
                 $scope.dirs = data;
-            });
+                });
 
-            // Parse breadcrumbs
-            var pathElements = path.split('/'), result = [], i;
-            var breadcrumbPath = function (index) {
-                return '/' + (pathElements.slice(0, index + 1)).join('/');
-            };
-            result.push({name: ".", path: "."});
-            pathElements.shift();
-            for (i=0; i<pathElements.length; i++) {
-                result.push({name: pathElements[i], path: breadcrumbPath(i)});
+                // Parse breadcrumbs
+                var pathElements = path.split('/'), result = [], i;
+                var breadcrumbPath = function (index) {
+                    return '/' + (pathElements.slice(0, index + 1)).join('/');
+                };
+                result.push({name: ".", path: "."});
+                pathElements.shift();
+                for (i=0; i<pathElements.length; i++) {
+                    result.push({name: pathElements[i], path: breadcrumbPath(i)});
+                }
+
+                $scope.breadcrumbs = result;
             }
 
-            $scope.breadcrumbs = result;
 	   });
 }]);
