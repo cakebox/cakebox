4070ebffb943c82097d4b25aa331bd3cfaf4faba
Tuxity <kevin.darcel@gmail.com>
2013-10-20 16:27:47 +0200

    Improve template

--- public/app/MediaCtrl.js
+++ public/app/MediaCtrl.js
@@ -2,4 +2,10 @@ cakeboxCtrl.controller('MediaCtrl', ['$scope', '$http', '$routeParams',
     function($scope, $http, $routeParams) {
 
         $scope.mediaPath = '/access/' + $routeParams.path;
+
+        /*
+        $http.get('/api/file/' + $routeParams.path).success(function(data) {
+            $scope.fileInfo = data;
+        });
+		*/
 }]);
