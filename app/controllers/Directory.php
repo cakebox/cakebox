c5ed7ab99b0e29c753f2108ecd7dc750f5fc4f37
Tuxity <kevin.darcel@gmail.com>
2013-10-20 19:28:59 +0200

    Correctly set directory size

--- app/controllers/Directory.php
+++ app/controllers/Directory.php
@@ -5,6 +5,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\Finder\Finder;
 
+function getDirSize($directory) {
+
+	$size = 0;
+	foreach(new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory)) as $file) {
+		$size += $file->getSize();
+    }
+
+    return $size;
+}
+
 $app->get("/api/directories/{path}", function (Request $request, $path) use ($app) {
 
 	$dirContent = array();
@@ -17,7 +27,7 @@ $app->get("/api/directories/{path}", function (Request $request, $path) use ($ap
 		$pathInfo = array();
 		$pathInfo["name"] = $file->getRelativePathname();
 		$pathInfo["type"] = $file->getType();
-		$pathInfo["size"] = $file->getSize();
+		$pathInfo["size"] = ($file->isFile()) ? $file->getSize() : getDirSize($file->getRealpath());
 		$pathInfo["perms"] = substr(sprintf('%o', $file->getPerms()), -4);
 		$pathInfo["isVideo"] = ( explode("/", mime_content_type($file->getRealpath()) )[0] == "video" || $file->getExtension() == "mkv") ? true : false;
 
