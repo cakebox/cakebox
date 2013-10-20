3d9b7da417141e011e2392e88d5099c441f990f6
Tuxity <kevin.darcel@gmail.com>
2013-10-20 17:15:15 +0200

    Add Missing NavCtrl file

---
+++ public/app/NavCtrl.js
@@ -0,0 +1,9 @@
+cakeboxCtrl.controller('NavCtrl', ['$scope', '$location',
+    function($scope, $location) {
+
+    	$scope.currentPage = "";
+
+       	$scope.$on('$routeChangeSuccess', function(event, current) {
+       		$scope.currentPage = $location.path().slice(1);
+    	});
+}]);
