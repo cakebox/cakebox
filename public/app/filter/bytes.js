fc2bfde4f00e1288360e73561099bfb717a884ea
Tuxity <kevin.darcel@gmail.com>
2013-10-21 21:36:58 +0200

    Fix bytes filter when bytes = 0

--- public/app/filter/bytes.js
+++ public/app/filter/bytes.js
@@ -1,9 +1,16 @@
 app.filter('bytes', function() {
-  return function(bytes, precision) {
-    if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
-    if (typeof precision === 'undefined') precision = 1;
-    var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
-      number = Math.floor(Math.log(bytes) / Math.log(1024));
-    return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
-  }
+
+    return function(bytes, precision) {
+
+        var units = ['octets', 'ko', 'Mo', 'Go', 'To', 'Po'];
+
+        if (bytes == 0 || isNaN(parseFloat(bytes)) || !isFinite(bytes))
+            return '-';
+
+        if (angular.isUndefined(precision))
+            precision = 1;
+
+        number = Math.floor(Math.log(bytes) / Math.log(1024));
+        return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) + ' ' + units[number];
+    }
 });
