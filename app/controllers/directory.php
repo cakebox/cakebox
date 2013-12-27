2f85dd1a41417e266ff4f113caa331db96286e19
Tuxity <kevin.darcel@gmail.com>
2013-12-27 12:32:21 +0100

    Fix when using symlink

--- app/controllers/directory.php
+++ app/controllers/directory.php
@@ -20,16 +20,24 @@ $app->get("/api/directories/content/{dir}", function (Request $request, $dir) us
 	$dirContent = array();
 
 	$finder = new Finder();
-	$finder->followLinks()->depth('< 1')->in($app["cakebox.root"] . $dir)->sortByType();
+	$finder->followLinks()->depth('< 1')->in("{$app['cakebox.root']}{$dir}")->sortByType();
 
 	foreach ($finder as $file) {
 
+		if ($file->isLink()) {
+			$linkTo = readlink("{$app['cakebox.root']}{$dir}{$file->getBasename()}");
+			if (file_exists($linkTo) == false)
+				continue;
+
+			$file = new SplFileInfo($linkTo);
+		}
+
 		$pathInfo = array();
-		$pathInfo["name"] = $file->getRelativePathname();
+		$pathInfo["name"] = $file->getBasename();
 		$pathInfo["type"] = $file->getType();
 		$pathInfo["size"] = ($file->isFile()) ? $file->getSize() : getDirSize($file->getRealpath());
 		$pathInfo["isVideo"] = ( explode("/", mime_content_type($file->getRealpath()) )[0] == "video" || $file->getExtension() == "mkv") ? true : false;
-		$pathInfo["access"] = $app["cakebox.access"] . $dir . $file->getRelativePathname();
+		$pathInfo["access"] = "{$app['cakebox.access']}{$dir}{$file->getBasename()}";
 
 		array_push($dirContent, $pathInfo);
 	}
