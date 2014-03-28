d68c4835b64b4f65b6bc1a72a7d5743b2653c060
Tuxity <kevin.darcel@gmail.com>
2014-03-28 14:47:42 +0100

    Add button to refresh directory content Cleanup a bit view and controller

--- public/app/controller/BrowseCtrl.js
+++ public/app/controller/BrowseCtrl.js
@@ -5,24 +5,32 @@ app.controller('BrowseCtrl', ['$scope', '$http', '$routeParams', 'breadcrumbs',
         $scope.breadcrumbs = breadcrumbs;
         $scope.informations = "Chargement des fichiers, veuillez patienter ...";
 
-        $scope.$watch('location.path()', function(event, current) {
+        function getDatas(path) {
 
-            if ($routeParams.path != "")
-                $scope.currentPath += $routeParams.path + "/";
-
-            $http.get('api/directory/content/' + $scope.currentPath)
+            $http.get('api/directory/content/' + path)
                 .success(function(data, status, headers, config) {
                     $scope.informations = "";
-                    $scope.currentTS = Math.round(new Date().getTime() / 1000);
-                    if (data.length == 0)
-                        $scope.dirs = "empty";
-                    else
-                        $scope.dirs = data;
+                    $scope.dirs = data;
                 })
                 .error(function(data, status, headers, config) {
                     $scope.informations = "Une erreur est survenue (Erreur " + status + ")";
                     console.error("Cakebox: API is unreachable on /api/directory/content/ (" + status + ")");
                 });
+        }
+
+        $scope.$watch('location.path()', function(event, current) {
+
+            $scope.dirs = [];
+            $scope.currentTS = Math.round(new Date().getTime() / 1000);
+
+            if ($routeParams.path != "")
+                $scope.currentPath += $routeParams.path + "/";
+
+            getDatas($scope.currentPath);
         });
+
+        $scope.refreshDatas = function() {
+            getDatas($scope.currentPath);
+        };
     }
 ]);
