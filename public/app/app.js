293369fd015ff1e7aa503a356abe058852cf5ebd
Tuxity <kevin.darcel@gmail.com>
2014-06-21 15:37:48 +0200

    Add and load Angular-resource

--- public/app/app.js
+++ public/app/app.js
@@ -1,32 +1,33 @@
 var app = angular.module('cakebox',  [
-  'ngRoute',
-  'ui.bootstrap'
+    'ngResource',
+    'ngRoute',
+    'ui.bootstrap'
 ]);
 
 app.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/', {
-        templateUrl: 'partials/browse.html',
-        controller:  'BrowseCtrl'
-      }).
-      when('/browse/', {
-        templateUrl: 'partials/browse.html',
-        controller:  'BrowseCtrl'
-      }).
-      when('/browse/:path*', {
-        templateUrl: 'partials/browse.html',
-        controller:  'BrowseCtrl'
-      }).
-      when('/play/:path*', {
-        templateUrl: 'partials/play.html',
-        controller:  'MediaCtrl'
-      }).
-      when('/about', {
-        templateUrl: 'partials/about.html'
-      }).
-      otherwise({
-        redirectTo: '/'
-      });
-  }
+    function($routeProvider) {
+        $routeProvider.
+        when('/', {
+            templateUrl: 'partials/browse.html',
+            controller:  'BrowseCtrl'
+        }).
+        when('/browse/', {
+            templateUrl: 'partials/browse.html',
+            controller:  'BrowseCtrl'
+        }).
+        when('/browse/:path*', {
+            templateUrl: 'partials/browse.html',
+            controller:  'BrowseCtrl'
+        }).
+        when('/play/:path*', {
+            templateUrl: 'partials/play.html',
+            controller:  'MediaCtrl'
+        }).
+        when('/about', {
+            templateUrl: 'partials/about.html'
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+    }
 ]);
