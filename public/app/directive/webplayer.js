6dcdfa386b5b4cb017606cf00af87e2f315ea77c
Tuxity <kevin.darcel@gmail.com>
2014-06-22 11:44:38 +0200

    webplayer directive now watch for playertype and url

--- public/app/directive/webplayer.js
+++ public/app/directive/webplayer.js
@@ -1,41 +1,48 @@
-app.directive('webplayer', ["$location", function ($location) {
-  return {
-    restrict: 'E',
-    replace: true,
-    scope: {
-      playertype: '@',
-      url: '@',
-      type: '@'
-    },
-    template: "<div class='video'></div>",
-    link: function (scope, element, attrs) {
+app.directive('webplayer', ["$location",
+    function ($location) {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                player: '@',
+                url: '@',
+                mimetype: '@'
+            },
+            template: "<div class='video'></div>",
+            link: function (scope, element, attrs) {
+                var $_current = element;
 
-        var $_current = element;
+                var action = function(data) {
+                    if (data.url) {
+                        scope.url = $location.protocol() + "://" + $location.host() +  data.url;
 
-        attrs.$observe('url', function(url) {
+                        var $_clone = element.clone(),
+                            content = '';
 
-            if (url) {
+                        if (scope.player == "html5") {
+                            content = '<video id="html5" src="' + scope.url + '" type="' + scope.mimetype + '" controls></video>';
+                        }
+                        else if (scope.player == "divx") {
+                            content = '<object id="divx" classid="clsid:67DABFBF-D0AB-41fa-9C46-CC0F21721616" codebase="http://go.divx.com/plugin/DivXBrowserPlugin.cab">';
+                            content += '<param name="custommode" value="none" />';
+                            content += '<param name="autoPlay" value="false" />';
+                            content += '<param name="src" value="' + scope.url  + '" />';
+                            content += '<embed type="video/divx" src="' + scope.url  + '" width="100%" height="100%" autoPlay="false" custommode="none" pluginspage="http://go.divx.com/plugin/download/" />';
+                            content += '</object>';
+                        }
+                        else if (scope.player == "vlc") {
+                            content = '<embed id="vlc" type="application/x-vlc-plugin" version="VideoLAN.VLCPlugin.2" target="' + scope.url + '" autoplay="yes" pluginspage="http://www.videolan.org"></embed>';
+                        }
 
-                scope.url = $location.protocol() + "://" + $location.host() +  url;
-
-                var $_clone = element.clone(),
-                    content = '';
-
-                if (scope.playertype == "html5") {
-                    content += '<video id="html5" src="' + scope.url + '" type="' + scope.type + '" controls></video>';
+                        $_current.replaceWith($_clone.html(content));
+                        $_current = $_clone;
+                    }
                 }
-                else if (scope.playertype == "divx") {
-                    content += '<object id="divx" classid="clsid:67DABFBF-D0AB-41fa-9C46-CC0F21721616" codebase="http://go.divx.com/plugin/DivXBrowserPlugin.cab">';
-                    content += '<param name="custommode" value="none" />';
-                    content += '<param name="autoPlay" value="false" />';
-                    content += '<param name="src" value="' + scope.url  + '" />';
-                    content += '<embed type="video/divx" src="' + scope.url  + '" width="100%" height="100%" autoPlay="false" custommode="none" pluginspage="http://go.divx.com/plugin/download/" />';
-                    content += '</object>';
-                }
-                else if (scope.playertype == "vlc") {
-                    content += '<embed id="vlc" type="application/x-vlc-plugin" version="VideoLAN.VLCPlugin.2" target="' + scope.url + '" autoplay="yes" pluginspage="http://www.videolan.org"></embed>';
-                }
-            }, true);
+
+                scope.$watch(function () {
+                    return {'player': attrs.player, 'url': attrs.url};
+                }, action, true);
+            }
         }
     }
-}]);
+]);
