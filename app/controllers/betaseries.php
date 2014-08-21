8a5d4480d9133914edfe542e9382cc99ea26fc3c
Tuxity <kevin.darcel@gmail.com>
2014-08-21 11:18:49 +0200

    BetaSeries improvements: Retrieve BetaSerie informations about an episode or a movie even if login or passwd isn't set Remove icon next to title if seen or not Better error check condition

--- app/controllers/betaseries.php
+++ app/controllers/betaseries.php
@@ -47,30 +47,31 @@ function get_infos(Application $app, $name) {
         $app->abort(403, "This user doesn't have the rights to retrieve episode informations.");
     }
 
-    $auth = fetch("/members/auth", [
-        "key"      => $app["bs.apikey"],
-        "login"    => $app["bs.login"],
-        "password" => md5($app["bs.passwd"])
-    ],  "POST");
+    $auth_params = ["key" => $app["bs.apikey"]];
 
-    if (!empty($auth->errors)) {
-        $app->abort(401, "BetaSeries: " . $auth->errors[0]->text);
+    // Logged in if login and passwd are set.
+    // If not, we can retrieve anyway episode or movie informations.
+    if (!empty($app["bs.login"]) && !empty($app["bs.passwd"])) {
+        $auth_params = array_merge($auth_params, [
+            "login"    => $app["bs.login"],
+            "password" => md5($app["bs.passwd"])
+        ]);
+
+        $auth = fetch("/members/auth", $auth_params, "POST");
+        if (!empty($auth->errors)) {
+            $app->abort(401, "BetaSeries: " . $auth->errors[0]->text);
+        }
+
+        print_r($auth);
+        $auth_params = array_merge($auth_params, ["token" => $auth->token]);
     }
 
     // First, check if it's a TV Show
-    $file_info = fetch("/episodes/scraper", [
-        "key"   => $app["bs.apikey"],
-        "token" => $auth->token,
-        "file" => $name
-    ]);
+    $file_info = fetch("/episodes/scraper", array_merge($auth_params, ["file" => $name]));
 
     // if it's not a TV Show it should be a Movie
     if (!empty($file_info->errors)) {
-        $file_info = fetch("/movies/scraper", [
-            "key"   => $app["bs.apikey"],
-            "token" => $auth->token,
-            "file" => $name
-        ]);
+        $file_info = fetch("/movies/scraper", array_merge($auth_params, ["file" => $name]));
     }
 
     return $app->json($file_info);
