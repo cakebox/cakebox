13ef92342e8b779bb401a7e2351c97190fe8d026
martialdidit <martial.puygrenier@gmail.com>
2014-12-22 14:10:02 +0100

    Refactor Apache scripts #125
    The version of Apache is now took into account
    Simplify 4 scripts 2 scripts -> alias and subdomain
    The two scripts use the webconf directory example to create an alias or subdomain

--- scripts/apache/apache2-alias.sh
+++ scripts/apache/apache2-alias.sh
@@ -17,11 +17,11 @@ echo "Avant de lancer ce script, assurez vous d'avoir une version d'Apache insta
 echo "Après l'éxécution du script, vous pourrez accéder à cakebox via un sous-domaine ex : yourdomain.com/cakebox/"
 read -p "Appuyez sur une touche pour continuer ou Ctrl-c pour annuler."
 
-read -p "Nom de l'alias (ex cakebox): " ALIAS
+read -e -p "Nom de l'alias (ex cakebox): " ALIAS
 echo "Quel est le répertoire d'installation de cakebox ? (ex /home/cakebox ) :"
-read CAKEBOXREP
+read -e -p CAKEBOXREP
 echo "Quel est le répertoire de scan de cakebox ? (ex : /home/video ) :" 
-read VIDEOREP
+read -e -p VIDEOREP
 
 echo "\n\nDeploiement des modules pour apache"
 read -p "Appuyer sur une touche pour continuer ..."
@@ -33,42 +33,16 @@ service apache2 restart
 
 echo "\n\nDeploiement de la cakebox sur apache"
 read -p "Appuyer sur une touche pour continuer ..."
+echo "\n\nDeploiement de la cakebox sur apache"
+read -p "Appuyer sur une touche pour continuer ..."
 
-echo '<VirtualHost *:80>
+IFS=''
+while read line
+do
+    eval echo "'$line'" >> > /etc/apache2/sites-available/$ALIAS.conf
+done < ../../webconf-example/apache2-alias.conf.example
 
-    Alias /'$ALIAS' '$CAKEBOXREP'/public/
-
-    <Directory "'$CAKEBOXREP'/public/">
-        Options Indexes MultiViews FollowSymLinks
-        AllowOverride All
-
-        Require all granted
-
-        # use debug instead of production to get more log
-        SetEnv APPLICATION_ENV production
-
-        RewriteEngine On
-        RewriteBase /'$ALIAS'/
-        RewriteCond %{REQUEST_FILENAME} !-f
-        RewriteRule ^ index.php [QSA,L]
-    </Directory>
-
-    Alias /access '$VIDEOREP'/
-    <Directory '$VIDEOREP'/>
-        Options -Indexes
-
-        Require all granted
-        Satisfy Any
-
-        Header set Content-Disposition "attachment"
-    </Directory>
-
-    ErrorLog "/var/log/apache2/'$ALIAS'-error.log"
-    CustomLog "/var/log/apache2/'$ALIAS'-access.log" common
-</VirtualHost>
-
-' > /etc/apache2/sites-available/$ALIAS.conf
-
+sed "s%\$ALIAS%$ALIAS%g;s%\$CAKEBOXREP%$CAKEBOXREP%g;s%\$VIDEOREP%$VIDEOREP%g" ../../webconf-example/apache2-alias.conf.example >/etc/apache2/sites-available/$ALIAS.conf
 
 a2ensite $ALIAS.conf
 /etc/init.d/apache2 restart
---
+++ scripts/apache/apache2-subdomain.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# _________         __         ___.                 
+# \_   ___ \_____  |  | __ ____\_ |__   _______  ___
+# /    \  \/\__  \ |  |/ // __ \| __ \ /  _ \  \/  /
+# \     \____/ __ \|    <\  ___/| \_\ (  <_> >    < 
+#  \______  (____  /__|_ \\___  >___  /\____/__/\_ \
+#         \/     \/     \/    \/    \/            \/
+# Version: Light
+# Author: 
+#       @Tuxity
+#       @martialdidit
+#
+# Script de création d'un sous domaine pour cakebox-light ex : cakebox.domain.com
+# 
+
+echo "Avant de lancer ce script, assurez vous d'avoir Apache installée et d'avoir déjà installé cakebox-light"
+echo "Après l'éxécution du script, vous pourrez accéder à cakebox via un sous-domaine ex : cakebox.yourdomain.com"
+read -p "Appuyez sur une touche pour continuer ou Ctrl-c pour annuler."
+
+read -e -p "Nom du sous-domaine  (cakebox.exemple.com): " SUBDOMAIN
+echo "Quel est le répertoire d'installation de cakebox ? (ex : /home/cakebox ) :"
+read -e -p CAKEBOXREP
+echo "Quel est le répertoire de scan de cakebox ? (ex : /home/video ) :" 
+read -e -p VIDEOREP
+
+
+echo "\n\nDeploiement des modules pour apache"
+read -p "Appuyer sur une touche pour continuer ..."
+
+a2enmod headers
+a2enmod rewrite
+
+service apache2 restart
+
+echo "\n\nDeploiement de la cakebox sur apache"
+read -p "Appuyer sur une touche pour continuer ..."
+
+IFS=''
+while read line
+do
+    eval echo "'$line'" >> /etc/apache2/sites-available/$SUBDOMAIN.conf
+done < ../../webconf-example/apache2-alias.conf.example
+
+sed "s%\$SUBDOMAIN%$SUBDOMAIN%g;s%\$CAKEBOXREP%$CAKEBOXREP%g;s%\$VIDEOREP%$VIDEOREP%g" ../../webconf-example/apache2-vhost.conf.example >/etc/apache2/sites-available/$SUBDOMAIN.conf
+
+a2ensite $SUBDOMAIN.conf
+/etc/init.d/apache2 restart
+
+echo "\n\nCréation du sous-domaine terminé, rendez-vous sur '$SUBDOMAIN' pour streamer !"
