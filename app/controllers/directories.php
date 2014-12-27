1ca7168794c1b41295625958c2988e96b95ba158
ahyear <kevindelouya@gmail.com>
2014-12-27 17:05:43 +0100

    fix security issue (thank to Zerdoumi for reporting it)

--- app/controllers/directories.php
+++ app/controllers/directories.php
@@ -29,6 +29,9 @@ function get_content(Application $app, Request $request) {
         $app->abort(400, "Missing parameters");
     }
 
+    if (realpath($dirpath)!= realpath($app["cakebox.access"].$dirpath)){
+       $app->abort(400, "Bad directory");
+    }
     $finder = new Finder();
     $finder->followLinks()
             ->depth('< 1')
