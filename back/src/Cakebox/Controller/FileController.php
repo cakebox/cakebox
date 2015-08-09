ebf8d62c29bba1372179e9cbe73d821a549c2faa
Leopold Jacquot <leopold.jacquot@gmail.com>
2015-08-09 21:38:21 +0200

    #88 Video suivante/précédente

--- back/src/Cakebox/Controller/FileController.php
+++ back/src/Cakebox/Controller/FileController.php
@@ -3,17 +3,22 @@
 namespace Cakebox\Controller;
 
 use Silex\Application;
-use Symfony\Component\HttpFoundation\Request;
+use SPLFileInfo;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
-class FileController
-{
+/**
+ * FileController
+ */
+class FileController {
+
     /**
      * Get file informations (size, name ...)
      *
-     * @param Application $app Silex Application
-     * @param Request $request Request parameters
+     * @param Application $app     Silex Application
+     * @param Request     $request Request parameters
      *
      * @return JsonResponse Object containing file informations
      */
@@ -29,25 +34,46 @@ class FileController
             $app->abort(400, "Missing parameters");
         }
 
-        $file     = new \SPLFileInfo("{$app['cakebox.root']}/{$filepath}");
+        $file = new SPLFileInfo("{$app['cakebox.root']}/{$filepath}");
 
         $fileinfo             = [];
-        $fileinfo["name"]     = $file->getBasename(".".$file->getExtension());
+        $fileinfo["name"]     = $file->getBasename("." . $file->getExtension());
         $fileinfo["fullname"] = $file->getFilename();
         $fileinfo["mimetype"] = mime_content_type($file->getPathName());
-        $fileinfo["access"]   = str_replace('%2F', '/', rawurlencode("{$app['cakebox.access']}/{$filepath}"));
+        $fileinfo["access"]   =
+            str_replace('%2F', '/', rawurlencode("{$app['cakebox.access']}/{$filepath}"));
         $fileinfo["size"]     = $file->getSize();
 
-        return $app->json($fileinfo);
+        $arrDirectory = $this->getCurrentDirectoryFiles($file, $app);
+
+        if (count($arrDirectory) > 1) {
+            $fileinfo['previousFile'] =
+                $this->arrayKeyRelative($arrDirectory, $file->getRealPath(), -1);
+
+            if ($fileinfo['previousFile']) {
+                $fileinfo['previousFile'] =
+                    str_replace($app['cakebox.root'], '', $fileinfo['previousFile']);
+            }
+
+            $fileinfo['nextFile'] = $this->arrayKeyRelative($arrDirectory, $file->getRealPath(), 1);
+
+            if ($fileinfo['nextFile']) {
+                $fileinfo['nextFile'] =
+                    str_replace($app['cakebox.root'], '', $fileinfo['nextFile']);
+            }
+        }
+
+        return $app->json($fileinfo, 200);
     }
 
     /**
      * Delete a file
      *
-     * @param Application $app Silex Application
-     * @param Request $request Request parameters
+     * @param Application $app     Silex Application
+     * @param Request     $request Request parameters
      *
-     * @return JsonResponse Array of objects, directory content after the delete process
+     * @return JsonResponse Array of objects, directory content after the
+     *                      delete process
      */
     public function delete(Application $app, Request $request) {
 
@@ -78,7 +104,58 @@ class FileController
         unlink($file);
 
         $subRequest = Request::create('/api/directories', 'GET', ['path' => dirname($filepath)]);
+
         return $app->handle($subRequest, HttpKernelInterface::SUB_REQUEST);
     }
 
+    /**
+     * Get files in the current $file directory
+     *
+     * @param SPLFileInfo $file
+     * @param Application $app
+     *
+     * @return array
+     */
+    private function getCurrentDirectoryFiles(SPLFileInfo $file, Application $app) {
+        $finder = new Finder();
+        $finder->files()->in($file->getPath())->depth('== 0')->ignoreVCS(true)
+               ->ignoreDotFiles($app['directory.ignoreDotFiles'])->notName($app["directory.ignore"])
+               ->filter(function ($curFile) use ($app) {
+                   /**
+                    * @var SPLFileInfo $curFile
+                    */
+                   if ($curFile->isReadable()
+                       && in_array(strtolower($curFile->getExtension()), $app['extension.video'])
+                   ) {
+                       return true;
+                   }
+
+                   return false;
+               })->sortByType();
+
+        return iterator_to_array($finder->getIterator());
+    }
+
+    /**
+     * Get desired offset in an array
+     *
+     * @param array      $array
+     * @param int|string $current_key
+     * @param int        $offset
+     * @param bool       $strict
+     *
+     * @return mixed     return desired offset, if in array, or false if not
+     */
+    private function arrayKeyRelative($array, $current_key, $offset = 1, $strict = true) {
+
+        $keys = array_keys($array);
+
+        $current_key_index = array_search($current_key, $keys, $strict);
+
+        if (isset($keys[$current_key_index + $offset])) {
+            return $keys[$current_key_index + $offset];
+        }
+
+        return false;
+    }
 }
--- front/src/app/play/play.controller.js
+++ front/src/app/play/play.controller.js
@@ -2,39 +2,46 @@
     'use strict';
 
     angular.module('cakebox')
-    .controller('PlayCtrl', function ($scope, $routeParams, $translate, File, Player, Betaseries) {
+        .controller('PlayCtrl', function ($scope, $routeParams, $translate, File, Player, Betaseries, App) {
 
-        $translate(['NOTIFICATIONS.BETASERIES_ERROR']).then(function (translations) {
-            $scope.betaseries_error = translations['NOTIFICATIONS.BETASERIES_ERROR'];
+            $translate(['NOTIFICATIONS.BETASERIES_ERROR']).then(function (translations) {
+                $scope.betaseries_error = translations['NOTIFICATIONS.BETASERIES_ERROR'];
+            });
+
+            $scope.app = App.get(null, function(data) {
+                $scope.player = data.player;
+            });
+
+            $scope.bsConfig = Betaseries.getConfig();
+
+            $scope.fileinfo = File.get({'path': $routeParams.path}, function(data) {
+                if ($scope.bsConfig.apikey) {
+                    $scope.betaseries = Betaseries.get({'filename': data.name});
+                }
+
+                if (data.previousFile) {
+                    $scope.prevplay = '#/play' + data.previousFile;
+                }
+                if (data.nextFile) {
+                    $scope.nextplay = '#/play' + data.nextFile;
+                }
+            });
+
+            $scope.watched = function (event, id) {
+                if ($scope.bsConfig.apikey && $scope.bsConfig.login && $scope.bsConfig.passwd) {
+                    $scope.betaseries = Betaseries.setWatched({'id': id});
+                } else {
+                    alertify.log($scope.betaseries_error, 'error', 6000);
+                }
+            };
+
+            $scope.unwatched = function (event, id) {
+                if ($scope.bsConfig.apikey && $scope.bsConfig.login && $scope.bsConfig.passwd) {
+                    $scope.betaseries = Betaseries.setUnwatched({'id': id});
+                } else {
+                    alertify.log($scope.betaseries_error, 'error', 6000);
+                }
+            };
         });
-
-        $scope.player = Player.get(null, function(data) {
-            data.default_type = data.available_types[data.default_type];
-        });
-
-        $scope.bsConfig = Betaseries.getConfig();
-
-        $scope.fileinfo = File.get({'path': $routeParams.path}, function(data) {
-            if ($scope.bsConfig.apikey) {
-                $scope.betaseries = Betaseries.get({'filename': data.name});
-            }
-        });
-
-        $scope.watched = function (event, id) {
-            if ($scope.bsConfig.apikey && $scope.bsConfig.login && $scope.bsConfig.passwd) {
-                $scope.betaseries = Betaseries.setWatched({'id': id});
-            } else {
-                alertify.log($scope.betaseries_error, 'error', 6000);
-            }
-        };
-
-        $scope.unwatched = function (event, id) {
-            if ($scope.bsConfig.apikey && $scope.bsConfig.login && $scope.bsConfig.passwd) {
-                $scope.betaseries = Betaseries.setUnwatched({'id': id});
-            } else {
-                alertify.log($scope.betaseries_error, 'error', 6000);
-            }
-        };
-    });
 
 })();
