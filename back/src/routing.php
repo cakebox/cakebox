7c52a2c39749b81c619908142132c185462fb228
Tuxity <kevin.darcel@gmail.com>
2015-09-20 17:57:45 -0500

    Back: don't use /api/ anymore in routes

--- back/src/routing.php
+++ back/src/routing.php
@@ -3,25 +3,25 @@
 /**
  * App routes
  */
-$app->get('/api/app', 'Cakebox\Controller\MainController::getAppInfos');
+$app->get('/app', 'Cakebox\Controller\MainController::getAppInfos');
 
 /**
  * Directories routes
  */
-$app->get('/api/directories',         'Cakebox\Controller\DirectoryController::get');
-$app->delete('/api/directories',      'Cakebox\Controller\DirectoryController::delete');
-$app->get('/api/directories/archive', 'Cakebox\Controller\DirectoryController::archive');
+$app->get('/directories',         'Cakebox\Controller\DirectoryController::get');
+$app->delete('/directories',      'Cakebox\Controller\DirectoryController::delete');
+$app->get('/directories/archive', 'Cakebox\Controller\DirectoryController::archive');
 
 /**
  * Files routes
  */
-$app->get('/api/files',    'Cakebox\Controller\FileController::get');
-$app->delete('/api/files', 'Cakebox\Controller\FileController::delete');
+$app->get('/files',    'Cakebox\Controller\FileController::get');
+$app->delete('/files', 'Cakebox\Controller\FileController::delete');
 
 /**
  * Betaseries routes
  */
-$app->get('/api/betaseries/config',          'Cakebox\Controller\BetaseriesController::getConfig');
-$app->get('/api/betaseries/info/{name}',     'Cakebox\Controller\BetaseriesController::getInfos');
-$app->post('/api/betaseries/watched/{id}',   'Cakebox\Controller\BetaseriesController::setWatched');
-$app->delete('/api/betaseries/watched/{id}', 'Cakebox\Controller\BetaseriesController::unsetWatched');
+$app->get('/betaseries/config',          'Cakebox\Controller\BetaseriesController::getConfig');
+$app->get('/betaseries/info/{name}',     'Cakebox\Controller\BetaseriesController::getInfos');
+$app->post('/betaseries/watched/{id}',   'Cakebox\Controller\BetaseriesController::setWatched');
+$app->delete('/betaseries/watched/{id}', 'Cakebox\Controller\BetaseriesController::unsetWatched');
