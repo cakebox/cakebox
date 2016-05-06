849c2550e939f3ea37998d0fe5409476604df572
mpgn <martial.puygrenier@gmail.com>
2016-05-06 20:29:42 +0200

    Fix loading multiple times conf file for no auth
    If user doesn't use auth the file is not loaded every time

--- app/models/utils.php
+++ app/models/utils.php
@@ -72,13 +72,16 @@ function check_cookie(Application $app, $cookie)
  */
 function get_infos(Application &$app, $username) {
 
-    foreach ($app['users'] as $user) {
-        if (isset($user['user.name']) && $user['user.name'] === $username) {
-            foreach ($user as $key => $value) {
-                $app[$key] = $value;
+    if ($app['users.auth']) {
+        foreach ($app['users'] as $user) {
+            if (isset($user['user.name']) && $user['user.name'] === $username) {
+                foreach ($user as $key => $value) {
+                    $app[$key] = $value;
+                }
+                return 1;
             }
-            return 1;
         }
+        return 0;
     }
-    return 0;
+    return 1;
 }
